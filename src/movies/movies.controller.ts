552456294ae659108bc8bf3746e959f10b58ef82
dev4us <bronxofsoul@gmail.com>
2021-07-25 10:44:28 +0900

    EP5. How to using PartialType

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,34 +1,35 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
-  constructor(private readonly moviesService: MoviesService){}
+  constructor(private readonly moviesService: MoviesService) { }
 
   @Get()
-  getAll(): Movie[]{
+  getAll(): Movie[] {
     return this.moviesService.getAll();
   }
 
   @Get("/:id")
-  getOne(@Param('id') movieID: number): Movie{
+  getOne(@Param('id') movieID: number): Movie {
     return this.moviesService.getOne(movieID);
   }
 
   @Post()
-  create(@Body() movieData: CreateMovieDto){
+  create(@Body() movieData: CreateMovieDto) {
     return this.moviesService.create(movieData);
   }
 
   @Delete("/:id")
-  remove(@Param('id') movieID: number){
+  remove(@Param('id') movieID: number) {
     return this.moviesService.deleteOne(movieID);
   }
 
   @Patch('/:id')
-  patch(@Param('id') movieID: number, @Body() updateData: CreateMovieDto){
+  patch(@Param('id') movieID: number, @Body() updateData: UpdateMovieDto) {
     return this.moviesService.update(movieID, updateData)
   }
 }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,39 +1,40 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
 export class MoviesService {
   private movies: Movie[] = [];
 
-  getAll(): Movie[]{
+  getAll(): Movie[] {
     return this.movies;
   }
 
-  getOne(id: number): Movie{
+  getOne(id: number): Movie {
     const movie = this.movies.find(movie => movie.id === +id);
 
-    if(!movie){
+    if (!movie) {
       throw new NotFoundException(`Movie with ID: ${id} not found.`)
     }
     return movie;
   }
 
-  deleteOne(id: number){
+  deleteOne(id: number) {
     this.getOne(id);
     this.movies = this.movies.filter(movie => movie.id !== +id);
   }
 
-  create(movieData: CreateMovieDto){
+  create(movieData: CreateMovieDto) {
     this.movies.push({
       id: this.movies.length + 1,
       ...movieData
     })
   }
 
-  update(id: number, updateData: CreateMovieDto){
+  update(id: number, updateData: UpdateMovieDto) {
     const movie = this.getOne(id);
     this.deleteOne(id);
-    this.movies.push({...movie, ...updateData});
+    this.movies.push({ ...movie, ...updateData });
   }
 }
